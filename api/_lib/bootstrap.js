57f468c59090feab7c5c5bcd12811f4a57cd59c3
ddhsgm <ddhsgm@gmail.com>
2026-03-16 10:27:56 +0900

    Update bootstrap.js

--- api/_lib/bootstrap.js
+++ api/_lib/bootstrap.js
@@ -1,26 +1,9 @@
-const fs = require("fs");
-const path = require("path");
-
-let bootstrapCache = null;
-let missingContactsCache = null;
-
-function readJsonOnce(filename) {
-  const fullPath = path.join(process.cwd(), filename);
-  return JSON.parse(fs.readFileSync(fullPath, "utf-8"));
-}
-
 function getBootstrapData() {
-  if (!bootstrapCache) {
-    bootstrapCache = readJsonOnce("bootstrap.json");
-  }
-  return bootstrapCache;
+  return require("../../bootstrap.json");
 }
 
 function getMissingContacts() {
-  if (!missingContactsCache) {
-    missingContactsCache = readJsonOnce("missing-contacts.json");
-  }
-  return missingContactsCache;
+  return require("../../missing-contacts.json");
 }
 
 module.exports = {
